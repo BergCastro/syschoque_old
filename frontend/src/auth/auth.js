96dfd74f86e920c7f6112c2c218df1adfe71ff56
Lindemberg <fireberg2500@hotmail.com>
2018-07-31 12:01:28 -0300

    login simplicado ok

--- frontend/src/auth/auth.js
+++ frontend/src/auth/auth.js
@@ -1,85 +1,87 @@
-//import './auth.css'
-import React, { Component } from 'react'
-import { reduxForm, Field } from 'redux-form'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-
-import { login, signup } from './authActions'
-import Row from '../common/layout/row'
-import Grid from '../common/layout/grid'
-import Messages from '../common/msg/messages'
-import Input from '../common/form/inputAuth'
-//import logo from './img/logo-def.png'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { login } from "./authActions";
+import Row from "../common/layout/row";
+import Grid from "../common/layout/grid";
+import Messages from "../common/msg/messages";
+import Input from "../common/form/inputAuth";
 
 class Auth extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { loginMode: true }
-    }
+  state = {
+    email: "",
+    password: ""
+  };
 
-    changeMode() {
-        this.setState({ loginMode: !this.state.loginMode })
-    }
+  handleEmailChange = event => {
+    console.log("email: " + event.target.value);
+    this.setState({ email: event.target.value });
+  };
 
-    onSubmit(values) {
-        const { login, signup } = this.props
-        this.state.loginMode ? login(values) : signup(values)
-    }
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
 
-    componentDidMount() {
-        const body = document.querySelector('body').className = 'login-page'
-        console.log(body)
-        
-    }
-   
+  onSubmit = event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    const { login } = this.props;
+    const values = { email, password };
+    login(values);
+  };
 
-    render() {
-        const { loginMode } = this.state
-        const { handleSubmit } = this.props
-        
-        return (
-            
-            <div className="login-box">
-                
-                
-                <div className="login-logo"><b>SYSChoque</b> </div>
-                <div className="login-box-body">
-                    <p className="login-box-msg">Bem vindo!</p>
-                    <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
-                        <Field component={Input} type="input" name="name"
-                            placeholder="Nome Completo" icon='user' hide={loginMode} />
-                        <Field component={Input} type="input" name="cargo"
-                            placeholder="Cargo" icon='user' hide={loginMode} />
-                        <Field component={Input} type="input" name="nomeGuerra"
-                            placeholder="Nome de Guerra" icon='user' hide={loginMode} />
-                        <Field component={Input} type="email" name="email"
-                            placeholder="E-mail" icon='envelope' />
-                        <Field component={Input} type="password" name="password"
-                            placeholder="Senha" icon='lock' />
-                        <Field component={Input} type="password" name="confirm_password"
-                            placeholder="Confirmar Senha" icon='lock' hide={loginMode} />
-                        <Row>
-                            <Grid cols="4">
-                                <button type="submit"
-                                    className="btn btn-primary btn-block btn-flat">
-                                    {loginMode ? 'Entrar' : 'Registrar'}
-                                </button>
-                            </Grid>
-                        </Row>
-                    </form>
-                    <br />
-                    <a onClick={() => this.changeMode()}>
-                        {loginMode ? 'Novo usuário? Registrar aqui!' :
-                            'Já é cadastrado? Entrar aqui!'}
-                    </a>
-                </div>
-                <Messages />
-            </div>
-            
-        )
-    }
+  componentDidMount() {
+    document.querySelector("body").className = "login-page";
+  }
+
+  render() {
+    return (
+      <div className="login-box">
+        <div className="login-logo">
+          <b>SYSChoque</b>{" "}
+        </div>
+        <div className="login-box-body">
+          <p className="login-box-msg">Bem vindo!</p>
+          <form onSubmit={this.onSubmit}>
+            <Input
+              type="input"
+              name="email"
+              placeholder="Insira o email"
+              icon="user"
+              hide={false}
+              onChange={this.handleEmailChange}
+            />
+
+            <Input
+              type="password"
+              name="password"
+              placeholder="Insira a senha"
+              icon="lock"
+              hide={false}
+              onChange={this.handlePasswordChange}
+            />
+
+            <Row>
+              <Grid cols="4">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block btn-flat"
+                >
+                  Entrar
+                </button>
+              </Grid>
+            </Row>
+          </form>
+          <br />
+        </div>
+        <Messages />
+      </div>
+    );
+  }
 }
 
-Auth = reduxForm({ form: 'authForm' })(Auth)
-const mapDispatchToProps = dispatch => bindActionCreators({ login, signup }, dispatch)
-export default connect(null, mapDispatchToProps)(Auth)
+const mapDispatchToProps = dispatch => bindActionCreators({ login }, dispatch);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Auth);
